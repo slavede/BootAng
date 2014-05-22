930bf059587c2117a761992cf2f077d74587446d
Slaven Tomac <slaven.tomac@gmail.com>
2014-05-22 22:23:53 +0200

    Added basic navigation

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,4 +1,34 @@
 'use strict';
 
 angular
-  .module('bootAngApp', []);
+.module('bootAngApp', ['ngRoute'])
+.config(['$routeProvider', function($routeProvider) {
+	$routeProvider
+		.when('/home', {
+			templateUrl: 'views/main.html',
+			controller: 'MainCtrl'
+		});
+}]).run(['$rootScope', function($rootScope) {
+	$rootScope.tabActive = {
+		home : true,
+		participants : false
+	};
+
+	$rootScope.$on('$locationChangeStart', function(scope, next) {
+		var splittedNext = next.split('#/');
+
+		if (splittedNext[1] === '') {
+			splittedNext[1] = 'home';
+		}
+
+		$rootScope.tabActive[splittedNext[1]] = true;
+
+		angular.forEach($rootScope.tabActive, function(value, key) {
+			if (key === splittedNext[1]) {
+				$rootScope.tabActive[key] = true;
+			} else {
+				$rootScope.tabActive[key] = false;
+			}
+		});
+	});
+}]);
